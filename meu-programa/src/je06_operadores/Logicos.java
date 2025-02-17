82bfdf18728a416af00d8f6faab2ae557cc34e7b
Paulo Cesar <pcesarandrade@icloud.com>
2025-02-17 11:41:04 -0300

    Estudo Operadores logicos

---
+++ meu-programa/src/je06_operadores/Logicos.java
@@ -0,0 +1,10 @@
+package je06_operadores;
+
+public class Logicos {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        System.out.println(a > 5 || b < 5);
+    }
+}
